6f077cf32cffef3b91a8e06a6a60d02439140d8c
andy suwandy <info@bytutorial.com>
2016-04-20 23:52:44 +1000

    Added files via upload
    version 1.1.0 is released

--- responsive.js
+++ responsive.js
@@ -3,6 +3,8 @@ Licensed under MIT
 copyright 2016 andy suwandy
 https://bytutorial.com/framework-and-scripts-library/responsive
 https://github.com/bytutorial
+v1.1 20 Apr 2016
+This script is purely for handling the mobile menu only, this can be ignored if you want to use the responsive css framework only or build your own menu navigation
 ****/
 (function ($) {
     $.extend({
@@ -32,21 +34,49 @@ https://github.com/bytutorial
 			});
 			
 			//window resize
-			$(window).resize(function(event){if($(this).width() > 764){$("ul.nav-menu").show();}});
+			$(window).resize(function(event){if($(this).width() > 764){$("ul.nav-menu").show();$("ul.nav-menu li ul").removeAttr("style");}else{resetMenuDisplay();}});
 			
 			if ($("#transparent-bg").length == 0) {
 				$("body").prepend("<div id='transparent-bg'></div>");
 				
 				$("#transparent-bg").on("click", function(){
 					resetMenuDisplay();
-				});
+				}); 
 			}
 			
 			function resetMenuDisplay(){
 				$("ul.nav-menu, #transparent-bg").hide();
 				$("body, ul.nav-menu").removeClass("right-slide left-slide");
 				$("ul.nav-menu").attr("slide", "0");
+				$("ul.nav-menu li ul").hide();
+				$("ul.nav-menu li span.arrow").removeClass("arrow-down");
 			}
+			
+			function setArrowCollapsable(){
+				//using jquery to loop each li element and automatically add an arrow span if the element contains a child of ul list
+				$('ul.nav-menu li').each(function(){
+					if ($(this).find('ul').length > 0)
+					{
+						$(this).addClass("has-child");
+						$(this).prepend("<span class='arrow'></span>");
+					}
+				});
+		
+				$('ul.nav-menu > li.has-child a').on("click", function(event){
+					var currentArrow = $(this).parent().find(" > span");
+					if($(currentArrow).length > 0){
+						if($(currentArrow).attr("class").indexOf("arrow-down") > 0){
+							$(currentArrow).removeClass("arrow-down");
+							$(currentArrow).parent().find(" > ul").slideUp();
+						}else{
+							$(currentArrow).addClass("arrow-down");
+							$(currentArrow).parent().find(" > ul").slideDown();
+						}
+					}
+				});
+			}
+			
+			setArrowCollapsable();
 		}
 	});
 })(jQuery);
